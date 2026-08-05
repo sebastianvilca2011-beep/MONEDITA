d1272fb9db8df49f7fe7ff07a1ea3523b20cff3e
sebastianvilca2011-beep <sebastianvilca2011@gmail.com>
2026-08-05 18:12:02 -0500

    Update codiguito.py
    conversacion de monedas A EUROS  SEBASTIAN VILCA

--- codiguito.py
+++ codiguito.py
@@ -1,10 +1,21 @@
 # ===========================================
-# CONVERSOR DE MONEDAS - VERSIÓN 1
-# Autor(a): Nombre del estudiante
+# CONVERSOR DE MONEDAS - VERSIÓN 2
+# Autor(a): SEBASTIAN VILCA
 # ===========================================
 
 soles = float(input("Ingrese la cantidad en soles: "))
 
-dolares = soles / 3.60
+print("1. dolares")
+print("2. euros")
 
-print("Equivale a", dolares, "dólares.")
+opcion = input("seleccione una opcion: ")
+
+if opcion == "1":
+  dolares = soles / 3.80
+  print("equivale a", dolares,"dolares.")
+
+elif opcion == "2":
+     euros = soles/ 4.20
+     print("equivale a", euros, "euros.")
+else:
+print("opcion no valida.")
