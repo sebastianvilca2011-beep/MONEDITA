dabd7ef2cfdd79143df32783f0374bc73421cb27
sebastianvilca2011-beep <sebastianvilca2011@gmail.com>
2026-08-05 18:03:51 -0500

    Add files via upload
    ARCHIVO LLAMADO CODIGUITO DE VILCA SEBASTIAN .PY

---
+++ codiguito.py
@@ -0,0 +1,10 @@
+# ===========================================
+# CONVERSOR DE MONEDAS - VERSIÓN 1
+# Autor(a): Nombre del estudiante
+# ===========================================
+
+soles = float(input("Ingrese la cantidad en soles: "))
+
+dolares = soles / 3.60
+
+print("Equivale a", dolares, "dólares.")
