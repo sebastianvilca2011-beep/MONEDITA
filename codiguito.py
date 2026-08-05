f9419f518647480dc01fe4af89691e85ced421c4
sebastianvilca2011-beep <sebastianvilca2011@gmail.com>
2026-08-05 18:14:49 -0500

    Update codiguito.py
    CONVERSACION DE LIBRAS ESTERLINAS

--- codiguito.py
+++ codiguito.py
@@ -1,5 +1,5 @@
 # ===========================================
-# CONVERSOR DE MONEDAS - VERSIÓN 2
+# CONVERSOR DE MONEDAS - VERSIÓN 3
 # Autor(a): SEBASTIAN VILCA
 # ===========================================
 
@@ -7,6 +7,7 @@ soles = float(input("Ingrese la cantidad en soles: "))
 
 print("1. dolares")
 print("2. euros")
+print("3. libras esterlinas")
 
 opcion = input("seleccione una opcion: ")
 
@@ -17,5 +18,10 @@ if opcion == "1":
 elif opcion == "2":
      euros = soles/ 4.20
      print("equivale a", euros, "euros.")
+
+elif opcion == "3":
+  libras  = soles / 4.80
+  print("equivale a", libras, "libras".)
+  
 else:
 print("opcion no valida.")
